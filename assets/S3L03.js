b00a6bdd86170fad99dfa708d3bfd73aa0f46257
AlessandroB92 <alessandrobasile1992@gmail.com>
2023-11-08 16:28:33 +0100

    Eliminato righe superflue

--- assets/S3L03.js
+++ assets/S3L03.js
@@ -1,25 +1,23 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const taskInput = document.getElementById("taskInput");
-    const addTaskButton = document.getElementById("addTask");
-    const taskList = document.getElementById("taskList");
+const taskInput = document.getElementById("taskInput");
+const addTaskButton = document.getElementById("addTask");
+const taskList = document.getElementById("taskList");
 
-    addTaskButton.addEventListener("click", function () {
-        const taskText = taskInput.value.trim();
-        if (taskText !== "") {
-            const li = document.createElement("li");
-            li.innerHTML = `<span>${taskText}</span><button class="delete"><ion-icon name="trash-outline"></ion-icon></button>`;
+addTaskButton.addEventListener("click", function () {
+  const taskText = taskInput.value.trim();
+  if (taskText !== "") {
+    const li = document.createElement("li");
+    li.innerHTML = `<span>${taskText}</span><button class="delete"><ion-icon name="trash-outline"></ion-icon></button>`;
 
-            li.addEventListener("click", function () {
-                li.classList.toggle("completed");
-            });
-
-            taskList.appendChild(li);
-            taskInput.value = "";
-
-            const deleteButton = li.querySelector(".delete");
-            deleteButton.addEventListener("click", function () {
-                taskList.removeChild(li);
-            });
-        }
+    li.addEventListener("click", function () {
+      li.classList.toggle("completed");
     });
+
+    taskList.appendChild(li);
+    taskInput.value = "";
+
+    const deleteButton = li.querySelector(".delete");
+    deleteButton.addEventListener("click", function () {
+      taskList.removeChild(li);
+    });
+  }
 });
