cb1efbcdd40973e7dde2326c9c8223454cb599e5
AlessandroB92 <alessandrobasile1992@gmail.com>
2023-11-08 15:56:50 +0100

    first commit

---
+++ assets/S3L03.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const taskInput = document.getElementById("taskInput");
+    const addTaskButton = document.getElementById("addTask");
+    const taskList = document.getElementById("taskList");
+
+    addTaskButton.addEventListener("click", function () {
+        const taskText = taskInput.value.trim();
+        if (taskText !== "") {
+            const li = document.createElement("li");
+            li.innerHTML = `<span>${taskText}</span><button class="delete"><ion-icon name="trash-outline"></ion-icon></button>`;
+
+            li.addEventListener("click", function () {
+                li.classList.toggle("completed");
+            });
+
+            taskList.appendChild(li);
+            taskInput.value = "";
+
+            const deleteButton = li.querySelector(".delete");
+            deleteButton.addEventListener("click", function () {
+                taskList.removeChild(li);
+            });
+        }
+    });
+});
